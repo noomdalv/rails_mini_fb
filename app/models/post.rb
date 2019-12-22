32776e5e1ba1098e5f95dcb7eb40bcb770046565
noomdalv <vladlunasan@gmail.com>
2019-12-22 13:35:17 -0500

    created 'Like' model and respective associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   validates :body, presence: true, length: { minimum: 10, maximum: 1000 }
   validates :user_id, presence: true
   default_scope { order('created_at DESC') }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :name, length: { minimum: 4, maximum: 50 }, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
