957149399e5885c2e5b315f5623067f11f13cdb1
sumancrest0001 <suman.crest0001@gmail.com>
2019-12-27 00:04:02 +0545

    created all models, controllers and model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
@@ -21,4 +23,30 @@ class User < ApplicationRecord
     like = Like.where('post_id = ? and user_id = ?', post.id, id)
     like.count.positive?
   end
+  # returns an array of friend who are friends with a particular user.
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.status}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.status}
+    friends_array.compact
+  end
+  # returns
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.status}.compact
+  end
+
+
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.status}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.status = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
 end
