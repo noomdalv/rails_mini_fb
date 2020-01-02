a934c7fe133162cb98e1c14b4aea2093d81a6d50
sumancrest0001 <suman.crest0001@gmail.com>
2020-01-02 21:15:28 +0545

    small changes on functionality

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@
 
 module UsersHelper
   # Returns the Gravatar for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, size)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
 
@@ -16,4 +16,9 @@ module UsersHelper
   def friends(user)
     current_user.sent_friends.include?(user.id) || current_user.received_friends.include?(user.id)
   end
+
+   def friend_profiles
+     ids = current_user.sent_friends
+     User.where(id:ids)
+   end
 end
